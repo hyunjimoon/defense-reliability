74a5e396d7ae113fb7a6ef9702a36a68e68c39ad
Hyunji Moon <30194633+hyunjimoon@users.noreply.github.com>
2020-11-13 14:17:11 +0900

    update model to state-dependent rate parameters

---
+++ optimize/step1/D_rate_M.stan
@@ -0,0 +1,71 @@
+data {
+  int<lower=0> N; // numbmer of observations
+  int<lower=1>n_state; // number of states
+  vector[5] state_obs[N];
+  int time_obs[N];
+  int max_allowed_state;
+  int repair_state;
+  int<lower=1, upper=n_state> initial_state;
+}
+
+transformed data {
+  vector[n_state] initial;
+  matrix[n_state, n_state] M;
+
+  for(i in 1:n_state){
+    initial[i] = 0;
+    if(i == initial_state){
+      initial[i] = 1;
+    }
+  }
+
+  for(i in 1:n_state){
+    for(j in 1:n_state){
+      M[i, j] = 0;
+    }
+  }
+  for(i in 1:max_allowed_state){
+    M[i, i] = 1;
+  }
+  for(i in (max_allowed_state+1):n_state){
+    M[i, repair_state] = 1;
+  }
+}
+
+
+parameters {
+  vector[n_state-1] rate;
+  //matrix[n_state,n_state] D_rate;
+  // simplex[5] state_1;
+  // simplex[4] state_2;
+  // simplex[3] state_3;
+  // simplex[2] state_4;
+}
+
+transformed parameters {
+  matrix[n_state, n_state] D_init = diag_matrix(rep_vector(1, n_state));
+  matrix[n_state, n_state] D_rate_c;
+  matrix[n_state, n_state] D_pow[max(time_obs)];
+  matrix[n_state, n_state] DM_pow[max(time_obs)];
+  for (i in 1:(n_state-1)){
+    D_rate_c[i,i] = -rate[i];
+    D_rate_c[i,i+1] = rate[i];
+  }
+  D_rate_c[n_state,n_state] =1;
+
+  # M should be multiplied in rate
+  for (i in 1:max(time_obs)){
+    D_pow[i] = scale_matrix_exp_multiply(i,  D_rate_c, D_init);
+    DM_pow[i] = scale_matrix_exp_multiply(i,  (D_rate_c * M), D_init);
+  }
+}
+
+model {
+  for(i in 1:N){
+    if (time_obs[i] ==1){
+      target += -(initial' * D_pow[time_obs[i]] - state_obs[i]'); #how to prevent DM_pow[0]?
+    }else{
+      target += -(initial' * DM_pow[time_obs[i]-1] * D_pow[time_obs[i]] - state_obs[i]'); #how to prevent DM_pow[0]?
+    }
+  }
+}
--- optimize/step1/approx_deterioration_matrix.R
+++ optimize/step1/approx_deterioration_matrix.R
@@ -1,14 +1,15 @@
 source(file.path(getwd(), "impute/mice_imputation.R"))
 scriptDir <- getwd()
 library(rstan)
-model <- stan_model(file.path(getwd(), "optimize/step1/approx_deterioration_matrix.stan"), verbose = TRUE)
+model <- stan_model(file.path(getwd(), "optimize/step1/D_rate_M.stan"), verbose = TRUE) #approx_deterioration_matrix
+
 
 
 mice_imp <- generateMice()
 imputed_data <- complete(mice_imp, 1)
 
 ####################################
-state_count = 5
+n_state = 5
 max_allowed_state = 3
 repair_state = 2
 initial_state = 1
@@ -31,7 +32,7 @@ generate_state_matrix <- function(data, n){
   matrix(state,nrow=31)
 }
 
-state_matrix <- generate_state_matrix(imputed_data$y_data, state_count)
+state_matrix <- generate_state_matrix(imputed_data$y_data, n_state)
 # one-hot encode to vector
 
 options(scipen = 999)
@@ -39,18 +40,19 @@ for(engine_type in 1:5){
   states <- as.vector(t(state_matrix))[imputed_data$engine_ind == engine_type]
   onehot <- list()
   for(i in 1:length(states)){
-    t_tmp <- as.vector(rep(0, len=state_count))
+    t_tmp <- as.vector(rep(0, len=n_state))
     t_tmp[states[i]] <- 1
     onehot[[i]] <- t_tmp
   }
-  opt_data <- list(N=length(onehot), n_states=state_count, state_obs=onehot, time_obs=imputed_data$age_ind[imputed_data$engine_ind == engine_type], max_allowed_state=max_allowed_state, repair_state=repair_state, initial_state=initial_state)
+  opt_data <- list(N=length(onehot), n_state=n_state, state_obs=onehot, time_obs=imputed_data$age_ind[imputed_data$engine_ind == engine_type], max_allowed_state=max_allowed_state, repair_state=repair_state, initial_state=initial_state)
   res <- optimizing(model, opt_data)
+  res["rate[1]"]
   print(paste0("return code:", res$return_code))
   res <- res$par
-  mat <- matrix(rep(0.0, n_states ** 2), nrow=state_count)
-  for(i in 1:state_count){mat[i,i] <- 1}
-  for(state in 1:(state_count-1)){
-    for(col in 1:(state_count+1-state)){
+  mat <- matrix(rep(0.0, n_states ** 2), nrow=n_state)
+  for(i in 1:n_state){mat[i,i] <- 1}
+  for(state in 1:(n_state-1)){
+    for(col in 1:(n_state+1-state)){
       mat[state, col + (state-1)] <- as.numeric(res[paste0("state_",state,"[",col,"]")])
     }
   }
@@ -60,4 +62,6 @@ for(engine_type in 1:5){
   print(mat)
   print(rowSums(mat))
 }
-options(scipen=0)
+
+#options(scipen=0)
+
