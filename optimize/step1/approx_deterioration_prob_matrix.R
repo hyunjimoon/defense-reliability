38bb8185d992ecc446e7bd7cf4070caa8c45e041
Dashadower <tttllshin@gmail.com>
2020-11-16 21:32:10 +0900

    Separate probability and rate approximation files, fix onehot_array

--- optimize/step1/approx_deterioration_prob_matrix.R
+++ optimize/step1/approx_deterioration_prob_matrix.R
@@ -2,7 +2,7 @@
 source(file.path(getwd(), "impute/mice_imputation.R"))
 scriptDir <- getwd()
 library(rstan)
-model <- stan_model(file.path(getwd(), "optimize/step1/D_rate_M.stan"), verbose = FALSE) #approx_deterioration_matrix
+model <- stan_model(file.path(getwd(), "optimize/step1/approx_deterioration_prob_matrix.stan"), verbose = FALSE) #approx_deterioration_matrix
 
 
 
@@ -34,18 +34,20 @@ generate_state_matrix <- function(data, n){
 }
 
 state_matrix <- generate_state_matrix(imputed_data$y_data, n_state)
-# one-hot encode to vector
+
 options(scipen = 999)
 
 for(engine_type in 1:5){
   states <- as.vector(t(state_matrix))[imputed_data$engine_ind == engine_type]
   onehot <- list()
+  # one-hot encode to vector
   for(i in 1:length(states)){
     t_tmp <- as.vector(rep(0, len=n_state))
     t_tmp[states[i]] <- 1
     onehot[[i]] <- t_tmp
   }
-  onehot_array<-array(unlist(onehot),c(length(onehot),n_state))
+  #onehot_array<-array(unlist(onehot),c(length(onehot),n_state))
+  onehot_array <- aperm(array(unlist(onehot),c(n_state, length(onehot))))
   opt_data <- list(N=length(onehot), n_state=n_state, state_obs=onehot_array, time_obs=imputed_data$age_ind[imputed_data$engine_ind == engine_type], max_allowed_state=max_allowed_state, repair_state=repair_state, initial_state=initial_state)
   print("start sampling")
   res <- optimizing(model, opt_data)#, init=list(rate=rep(1.0, n_state-1)))
@@ -53,25 +55,19 @@ for(engine_type in 1:5){
   res["rate[1]"]
   print(paste0("return code:", res$return_code))
   res <- res$par
-  # mat <- matrix(rep(0.0, n_states ** 2), nrow=n_state)
-  # for(i in 1:n_state){mat[i,i] <- 1}
-  # for(state in 1:(n_state-1)){
-  #   for(col in 1:(n_state+1-state)){
-  #     mat[state, col + (state-1)] <- as.numeric(res[paste0("state_",state,"[",col,"]")])
-  #   }
-  # }
-  rate_matrix <- matrix(rep(0.0, n_state ** 2), nrow=n_state)
-  for(i in 1:(n_state-1)){
-    rate_matrix[i, i] = -as.numeric(res[paste0("rate","[",i,"]")]);
-    rate_matrix[i, i+1] = as.numeric(res[paste0("rate","[",i,"]")]);
+  mat <- matrix(rep(0.0, n_states ** 2), nrow=n_state)
+  for(i in 1:n_state){mat[i,i] <- 1}
+  for(state in 1:(n_state-1)){
+    for(col in 1:(n_state+1-state)){
+      mat[state, col + (state-1)] <- as.numeric(res[paste0("state_",state,"[",col,"]")])
+    }
   }
-  rate_matrix[n_state, n_state] = 1
   print("###########")
   print(engine_type)
-  #mat <- t(matrix(mat, nrow = 5))
-  #print(mat)
-  #print(rowSums(mat))
-  print(rate_matrix)
+  mat <- t(matrix(mat, nrow = 5))
+  print(mat)
+  print(rowSums(mat))
+  
 }
 
 #options(scipen=0)
---
+++ optimize/step1/approx_deterioration_rate_matrix.R
@@ -0,0 +1,70 @@
+#setwd("C:/Users/serim/Documents/GitHub/reliability_prediction")
+source(file.path(getwd(), "impute/mice_imputation.R"))
+scriptDir <- getwd()
+library(rstan)
+model <- stan_model(file.path(getwd(), "optimize/step1/approx_deterioration_rate_matrix.stan"), verbose = FALSE) #approx_deterioration_matrix
+
+
+
+mice_imp <- generateMice()
+imputed_data <- complete(mice_imp, 1)
+
+####################################
+n_state = 5
+max_allowed_state = 3
+repair_state = 2
+initial_state = 1
+
+generate_maintenance_matrix <- function(n_states, max_allowed_state, repair_state){
+  mat <- matrix(rep(0, len=n_states ** 2), nrow=n_states)
+  for(i in 1:max_allowed_state){
+    mat[i, i] <- 1
+  }
+  for(i in max_allowed_state+1:n_states){
+    mat[i, repair_state] <- 1
+  }
+  
+  return(mat)
+}
+
+generate_state_matrix <- function(data, n){
+  state<-cut(data, breaks=c(quantile(data,seq(0,1,length.out = n+1))),labels = 1:n, include.lowest=TRUE)
+  state<-as.numeric(as.character(state))
+  matrix(state,nrow=31)
+}
+
+state_matrix <- generate_state_matrix(imputed_data$y_data, n_state)
+
+options(scipen = 999)
+
+for(engine_type in 1:5){
+  states <- as.vector(t(state_matrix))[imputed_data$engine_ind == engine_type]
+  onehot <- list()
+  # one-hot encode per-data state to vector
+  for(i in 1:length(states)){
+    t_tmp <- as.vector(rep(0, len=n_state))
+    t_tmp[states[i]] <- 1
+    onehot[[i]] <- t_tmp
+  }
+  #onehot_array<-array(unlist(onehot),c(length(onehot),n_state))  # array() fills column-wise first
+  onehot_array <- aperm(array(unlist(onehot),c(n_state, length(onehot))))
+  opt_data <- list(N=length(onehot), n_state=n_state, state_obs=onehot_array, time_obs=imputed_data$age_ind[imputed_data$engine_ind == engine_type], max_allowed_state=max_allowed_state, repair_state=repair_state, initial_state=initial_state)
+  print("start sampling")
+  res <- optimizing(model, opt_data)#, init=list(rate=rep(1.0, n_state-1)))
+  print("end sampling")
+  res["rate[1]"]
+  print(paste0("return code:", res$return_code))
+  res <- res$par
+  rate_matrix <- matrix(rep(0.0, n_state ** 2), nrow=n_state)
+  for(i in 1:(n_state-1)){
+    rate_matrix[i, i] = -as.numeric(res[paste0("rate","[",i,"]")]);
+    rate_matrix[i, i+1] = as.numeric(res[paste0("rate","[",i,"]")]);
+  }
+  rate_matrix[n_state, n_state] = 1
+  print("###########")
+  print(engine_type)
+  print(rate_matrix)
+}
+
+#options(scipen=0)
+
